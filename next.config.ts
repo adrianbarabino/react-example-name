273c9b9a915d226be8f97524dc311579932a2e53
Adrian Barabino <adrian.barabino@gmail.com>
2024-11-16 16:47:00 -0300

    Initial Example React

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Habilita la exportación estática
+  trailingSlash: true, // Opcional: genera `index.html` en subcarpetas
+  basePath: '/browserx', // Cambiá '/browserx' por tu subdirectorio
+    distDir: 'out' // Opcional: cambia el nombre del directorio de salida
 };
+
+module.exports = nextConfig;
+
 
 export default nextConfig;
